98f5c2769668b0bc3c70af45a237f0d33bf196e7
Vaishnavi T V <140780960+TVVaishnavi@users.noreply.github.com>
2026-01-08 15:00:59 +0530

    Pass login state to Header and Body components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import { useState } from "react";
 import Header from './components/Header';
 import Body from './components/Body';
 
-
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    !!localStorage.getItem("usertoken")
+  );
+
   return (
     <div className="App">
-      <Header />
-      <Body />
+      <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+      <Body setIsLoggedIn={setIsLoggedIn} />
     </div>
   );
 }

899fb3aa49ef58ec6928c833132986c81a49d613
Vaishnavi T V <140780960+TVVaishnavi@users.noreply.github.com>
2026-01-08 14:28:18 +0530

    Implement logout feature in Header component
    Added logout functionality to the Header component.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,21 @@ import { useNavigate } from "react-router-dom";
 
 function Header() {
     const navigate = useNavigate();
+    const token = localStorage.getItem("token");
+
+    const handleLogout = () =>{
+        localStorage.removeItem("token");
+        navigate("/login")
+    }
     return (
         <div className='header-container'>
-            <h1 onClick={()=>navigate("/")}>Task Viewer</h1>
+            <h1 onClick={()=>navigate("/")}>Task Viewer</h1> 
+
+            {token && (
+                <button onClick={handleLogout}>
+                Logout
+                </button>
+            )}
         </div>
     )
 

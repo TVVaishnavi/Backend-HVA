0c7addfec80d0ae4d12afa5e7860346ffde62d10
Vaishnavi T V <140780960+TVVaishnavi@users.noreply.github.com>
2026-01-09 15:15:16 +0530

    Add close button to Admin Login page

--- client/src/pages/AdminLogin.js
+++ client/src/pages/AdminLogin.js
@@ -40,6 +40,7 @@ function AdminLogin({setIsLoggedIn }) {
     return (
         <div className="login-container">
             <div className="login-card">
+               <div className="close-btn" onClick={() => navigate("/")}>X</div>
                 <h2>Admin Login</h2>
 
                 <div className="input-group">

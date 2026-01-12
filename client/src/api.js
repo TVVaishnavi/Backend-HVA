db55f54a487e08367f98f05a5efccd97270e6870
Vaishnavi T V <140780960+TVVaishnavi@users.noreply.github.com>
2026-01-12 14:50:40 +0530

    Clarify baseURL in api.js
    Updated the baseURL comment for clarity.

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://backend-hva.onrender.com',
+  baseURL: 'https://backend-hva.onrender.com', //http://13.203.67.3:5000 (AWS EC2 - one which i deployed it)
+
 });
 
 api.interceptors.request.use((config) => {
